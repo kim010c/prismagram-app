17c85553e9f49a5753e0446d708a035f7d278bfe
Park Jongho <kim010c@gmail.com>
2019-12-19 15:25:34 +0900

    #11-4 Messages Navigation part One

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,19 +1,50 @@
-import { View } from "react-native";
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createStackNavigator } from "react-navigation-stack";
 import Home from "../screens/Home";
 import Search from "../screens/Search";
 import Notifications from "../screens/Notifications";
 import Profile from "../screens/Profile";
 
+const stackFactory = (initialRoute, customConfig) =>
+  createStackNavigator({
+    InalRoute: {
+      screen: initialRoute,
+      navigationOptions: { ...customConfig }
+    }
+  });
+
 export default createBottomTabNavigator({
-  Home,
-  Search,
+  Home: {
+    screen: stackFactory(Home, {
+      title: "Home",
+      headerRight: (
+        <TouchableOpacity>
+          <Text>Hello</Text>
+        </TouchableOpacity>
+      )
+    })
+  },
+  Search: {
+    screen: stackFactory(Search, {
+      title: "Search"
+    })
+  },
   Add: {
     screen: View,
     navigationOptions: {
       tabBarOnPress: ({ navigation }) => navigation.navigate("PhotoNavigation")
     }
   },
-  Notifications,
-  Profile
+  Notifications: {
+    screen: stackFactory(Notifications, {
+      title: "Notifications"
+    })
+  },
+  Profile: {
+    screen: stackFactory(Profile, {
+      title: "Profile"
+    })
+  }
 });
