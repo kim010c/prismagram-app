283b4dc9855015774e7e83b9425d7e753228cfdf
Park Jongho <kim010c@gmail.com>
2019-12-26 15:13:25 +0900

    #15-1 Post Component Header

--- screens/Home.js
+++ screens/Home.js
@@ -4,6 +4,7 @@ import { gql } from "apollo-boost";
 import Loader from "../components/Loader";
 import { useQuery } from "react-apollo-hooks";
 import { ScrollView, RefreshControl } from "react-native";
+import Post from "../components/Post";
 
 const FEED_QUERY = gql`
   {
@@ -35,13 +36,6 @@ const FEED_QUERY = gql`
   }
 `;
 
-const View = styled.View`
-  justify-content: center;
-  align-items: center;
-  flex: 1;
-`;
-const Text = styled.Text``;
-
 export default () => {
   const [refreshing, setRefreshing] = useState(false);
   const { loading, data, refetch } = useQuery(FEED_QUERY);
@@ -61,7 +55,13 @@ export default () => {
         <RefreshControl refreshing={refreshing} onRefresh={refresh} />
       }
     >
-      {loading ? <Loader /> : <Text>Hello</Text>}
+      {loading ? (
+        <Loader />
+      ) : (
+        data &&
+        data.seeFeed &&
+        data.seeFeed.map(post => <Post key={post.id} {...post} />)
+      )}
     </ScrollView>
   );
 };
